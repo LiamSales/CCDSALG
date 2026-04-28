c2f8014cfb2344cde4e1cf13d10e6a67b0ff87f7
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-04-28 22:09:17 +0800

    ok sorting works

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        String path = "../data/random100.txt";
+        String path = "../data/random100000.txt";
         FileReader fileReader = new FileReader();
         Record[] arr = fileReader.readFile(path);
 
