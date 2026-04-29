47373d354fa61ce8e7925fa1e1f80f8aea6c39a6
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-04-29 20:09:27 +0800

    tested them all

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
 
-        String path = "../data/random100000.txt";
+        String path = "../data/merge.txt";
         FileReader fileReader = new FileReader();
         Record[] arr = fileReader.readFile(path);
 
