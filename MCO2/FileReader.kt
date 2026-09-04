55a92073de192e7aadee7b413a83bd9edaa8f49e
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-09-04 21:34:56 +0800

    Create MCO2/FileReader.kt

---
+++ MCO2/FileReader.kt
@@ -0,0 +1,36 @@
+import java.io.File
+
+class FileReader {
+
+    fun readFile(path: String): Array<MutableList<Int>> {
+
+        val file = File(path)
+        val scanner = file.bufferedReader()
+
+        // First line: n e
+        val firstLine = scanner.readLine().split(" ")
+
+        val n = firstLine[0].toInt()
+        val e = firstLine[1].toInt()
+
+        // Create a list for every account
+        val graph = Array(n) { mutableListOf<Int>() }
+
+        // Read all edges
+        repeat(e) {
+
+            val line = scanner.readLine().split(" ")
+
+            val a = line[0].toInt()
+            val b = line[1].toInt()
+
+            // Friendship is bidirectional
+            graph[a].add(b)
+            graph[b].add(a)
+        }
+
+        scanner.close()
+
+        return graph
+    }
+}
