951c730a8dad0f73014beeeb75857a243e3042e2
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-09-08 22:12:17 +0800

    Update Main.kt

--- MCO2/Main.kt
+++ MCO2/Main.kt
@@ -1,3 +1,40 @@
-// filereader
+fun main() {
 
-// menu option for 2 other settings (they use the reader anyway)
+    // Ask for dataset path
+    print("Enter file path: ")
+    val path = readLine()!!
+
+    // Read and build graph
+    val fileReader = FileReader()
+    val graph = fileReader.readFile(path)
+
+    // Main menu
+    while (true) {
+
+        println()
+        println("1. View person's friends")
+        println("2. View connection between two people")
+        println("3. Exit")
+        print("Choose an option: ")
+
+        when (readLine()) {
+
+            "1" -> {
+                // TODO: Implement friend list
+            }
+
+            "2" -> {
+                // TODO: Implement connection/path
+            }
+
+            "3" -> {
+                println("Exiting...")
+                break
+            }
+
+            else -> {
+                println("Invalid option.")
+            }
+        }
+    }
+}
