d63ec878198b62c10c662fcd55f576850368a0ab
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-09-09 14:27:27 +0800

    ok i get it perfectly now

--- MCO2/FileReader.kt
+++ MCO2/FileReader.kt
@@ -6,17 +6,14 @@ class FileReader {
 
         val file = File(path)
         val reader = file.bufferedReader()
-
-        // First line: n e
+        
         val firstLine = reader.readLine().trim().split(Regex("\\s+"))
 
         val n = firstLine[0].toInt()
         val e = firstLine[1].toInt()
 
-        // graph[id] = list of friends
         val graph = Array(n) { mutableListOf<Int>() }
 
-        // Read every edge
         repeat(e) {
 
             val line = reader.readLine().trim().split(Regex("\\s+"))
@@ -24,7 +21,6 @@ class FileReader {
             val a = line[0].toInt()
             val b = line[1].toInt()
 
-            // Bidirectional friendship
             graph[a].add(b)
             graph[b].add(a)
         }
