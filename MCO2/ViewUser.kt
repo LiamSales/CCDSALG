f47c1efeabe1609d05a57e56b8f58aecec0c6f35
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-09-10 20:32:53 +0800

    Update ViewUser.kt

--- MCO2/ViewUser.kt
+++ MCO2/ViewUser.kt
@@ -1,5 +1,22 @@
-//parameter ID
+class ViewUser {
 
-//output name, array size
+    fun displayFriendList(graph: Array<MutableList<Int>>, id: Int) {
 
-//output list (match id number with names)
+        if (id < 0 || id >= graph.size) {
+            println("Invalid ID.")
+            return
+        }
+
+        val friends = graph[id]
+
+        println("Person $id has ${friends.size} friends!")
+
+        print("List of friends: ")
+
+        for (friend in friends) {
+            print("$friend ")
+        }
+
+        println()
+    }
+}
