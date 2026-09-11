eeafa733c2a2623b41588a79e19969ee76704561
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-09-11 20:47:05 +0800

    Create ViewConnection.kt

---
+++ MCO2/ViewConnection.kt
@@ -0,0 +1,19 @@
+class ViewConnection {
+
+    fun findConnection(
+        graph: Array<MutableList<Int>>,
+        id1: Int,
+        id2: Int
+    ) {
+
+        // TODO: Validate id1 and id2
+
+        // TODO: Create/use Bidirectional BFS
+
+        // TODO: Find connection between id1 and id2
+
+        // TODO: Display connection if one exists
+
+        // TODO: Display message if no connection exists
+    }
+}
