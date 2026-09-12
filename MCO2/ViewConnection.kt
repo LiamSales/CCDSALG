9798258469540d65a6b385aaadaf10f590396ef1
Liam Miguel V. Sales <liam_miguel_sales@dlsu.edu.ph>
2026-09-12 20:18:25 +0800

    Update ViewConnection.kt
    review this in desktop, cant in mobile

--- MCO2/ViewConnection.kt
+++ MCO2/ViewConnection.kt
@@ -15,22 +15,65 @@ id2 < 0 || id2 >= graph.size ) {
             return
         }
 
-if (start == target) return 0
-
-val queueStart = ArrayDeque<Node>().apply { add(start) }
-    val queueTarget = ArrayDeque<Node>().apply { add(target) }
-
-    // Visited sets with distance tracking
-    val visitedStart = mutableMapOf(start to 0)
-    val visitedTarget = mutableMapOf(target to 0)
-
-
-
-
+if (id1 == id2) return
 
 
         // TODO: Display connection if one exists
 
         // TODO: Display message if no connection exists
     }
+
+
+
+fun bidirectionalBfs(start: Node, target: Node): Int {
+
+
+    // Queues for traversing levels
+    val queueStart = ArrayDeque<Node>().apply { add(start) }
+    val queueTarget = ArrayDeque<Node>().apply { add(target) }
+
+    // Visited sets with distance tracking
+    val visitedStart = mutableMapOf(start to 0)
+    val visitedTarget = mutableMapOf(target to 0)
+
+    while (queueStart.isNotEmpty() && queueTarget.isNotEmpty()) {
+        // Expand from the start side
+        val distFromStart = expandLevel(queueStart, visitedStart, visitedTarget)
+        if (distFromStart != -1) return distFromStart
+
+        // Expand from the target side
+        val distFromTarget = expandLevel(queueTarget, visitedTarget, visitedStart)
+        if (distFromTarget != -1) return distFromTarget
+    }
+
+    return -1 // No path found
+}
+
+private fun expandLevel(
+    queue: ArrayDeque<Node>,
+    visitedThisSide: MutableMap<Node, Int>,
+    visitedThatSide: Map<Node, Int>
+): Int {
+    val size = queue.size
+    for (i in 0 until size) {
+        val current = queue.removeFirst()
+        val currentDist = visitedThisSide[current]!!
+
+        for (neighbor in current.neighbors) {
+            // If the other search already visited this neighbor, we met!
+            if (visitedThatSide.containsKey(neighbor)) {
+                return currentDist + visitedThatSide[neighbor]!! + 1
+            }
+
+            if (!visitedThisSide.containsKey(neighbor)) {
+                visitedThisSide[neighbor] = currentDist + 1
+                queue.add(neighbor)
+            }
+        }
+    }
+    return -1
+}
+
+
+
 }
